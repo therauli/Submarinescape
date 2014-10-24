66d32c0668c55c69e738da9a64e0f929a64b870d
vayrytu <tuure.vayrynen@gmail.com>
2014-10-25 00:30:16 +0300

    Added graphics for Ellipsoid and Triangloid

--- src/resource.js
+++ src/resource.js
@@ -2,7 +2,9 @@ var res = {
     HelloWorld_png : "res/HelloWorld.png",
     CloseNormal_png : "res/CloseNormal.png",
     CloseSelected_png : "res/CloseSelected.png",
-    shapoid_ball_png : "res/shapoid_ball.png"
+    shapoid_ball_png : "res/shapoid_ball.png",
+    ellipsoid_png : "res/Ellipsoid.png",
+    triangloid_png : "res/Triangloid.png"
 };
 
 var g_resources = [];
